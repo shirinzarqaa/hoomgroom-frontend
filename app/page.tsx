f2f389a579d8e11b5c5da22b6ec8ba2bc1500c73
VirgilliaYeala <virgillia.yeala@ui.ac.id>
2024-05-25 14:28:03 +0700

    feat(navbar): adding navbar for hoomgroom app

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 export default function Home() {
   return (
-      <main className={"bg-primary min-h-screen flex flex-col items-center justify-center gap-4"}>
-          <h2 className={"text-4xl font-bold text-blue-primary"}>
+    <main className="bg-primary min-h-screen flex flex-col items-center justify-center gap-4">
+      <h2 className={"text-4xl font-bold text-blue-primary"}>
               HoomGroom
-          </h2>
-      </main>
+     </h2>
+      <Link href="/login" className="hover:scale-105 active:scale-95 active:opacity-70 transition-all bg-blue-primary w-28 justify-center flex rounded-lg py-1.5 font-semibold">
+        Login
+      </Link>
+      <Link href="/register" className="hover:scale-105 active:scale-95 active:opacity-70 transition-all bg-blue-primary w-28 justify-center flex rounded-lg py-1.5 font-semibold">
+        Register
+      </Link>
+     
+    </main>
   );
 }
