e316bed02cf9505bb5a8d3de5328119412f5d42e
HanifRifky <114400903+HanifRifky@users.noreply.github.com>
2024-05-26 20:06:08 +0700

    feat(pengiriman) Generate Kode Resi saat status pengiriman sudah Dikirim

--- app/pengiriman/page.tsx
+++ app/pengiriman/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { createPengiriman, updatePengirimanState, updatePengirimanTransportation } from './pengiriman';
+import { createPengiriman, updatePengirimanState, updatePengirimanTransportation } from './api/pengiriman';
 import React, { useState } from 'react';
 
 interface Product {
