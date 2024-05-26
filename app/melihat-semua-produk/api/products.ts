b23538e7c2ea9a4cc4b52a7d4c57cfb9cf49c4de
Shirin Zarqaa <shirinzarqaa354@gmail.com>
2024-05-26 16:55:35 +0700

    fix filter by api

--- app/melihat-semua-produk/api/products.ts
+++ app/melihat-semua-produk/api/products.ts
@@ -1,8 +1,7 @@
 
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/products';
-
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export async function getAllProducts() {
   try {
